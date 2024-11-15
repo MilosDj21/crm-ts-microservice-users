4700b2c8e0c45dba9bf82ee27999c00fee323500
Milos <m.djuka@hotmail.com>
2024-11-15 20:47:18 +0100

    add error handling middleware

---
+++ src/middlewares/error-handling.ts
@@ -0,0 +1,24 @@
+import { Request, Response, NextFunction } from "express";
+import { CustomError } from "./CustomError";
+
+export const errorHandler = (
+  err: any,
+  req: Request,
+  res: Response,
+  next: NextFunction,
+) => {
+  console.log(err);
+  if (err instanceof CustomError) {
+    res.status(err.statusCode).json({
+      error: err.message,
+    });
+    return;
+  }
+
+  // Handle unexpected errors
+  console.error("Unexpected error:", err);
+  res.status(500).json({
+    error: "Internal Server Error",
+  });
+  return;
+};

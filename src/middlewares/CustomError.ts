52426421252716d5ed03c6df56e010275f6a1bce
Milos <m.djuka@hotmail.com>
2024-11-19 19:21:35 +0100

    docs: add docs for custom error classes

--- src/middlewares/CustomError.ts
+++ src/middlewares/CustomError.ts
@@ -8,12 +8,16 @@ class CustomError extends Error {
     Object.setPrototypeOf(this, CustomError.prototype);
   }
 }
-
+/**
+ * Custom error class for "Bad Request" errors (HTTP 400)
+ * @class
+ * @extends {CustomError}
+ */
 class BadRequestError extends CustomError {
   /**
-   * Creates Bad Request Error
-   * @param message - this is sent back to client
-   * @param desc - this is used for logging
+   * Creates an instance of BadRequestError
+   * @param message - sent back to client
+   * @param desc - used for logging
    */
   constructor(message: string, desc: string = "") {
     super(message, 400, desc);
@@ -21,14 +25,34 @@ class BadRequestError extends CustomError {
   }
 }
 
+/**
+ * Custom error class for "Not Found" errors (HTTP 404)
+ * @class
+ * @extends {CustomError}
+ */
 class NotFoundError extends CustomError {
+  /**
+   * Creates an instance of NotFoundError
+   * @param message - sent back to client
+   * @param desc - used for logging
+   */
   constructor(message: string, desc: string = "") {
     super(message, 404, desc);
     Object.setPrototypeOf(this, NotFoundError.prototype);
   }
 }
 
+/**
+ * Custom error class for "Unauthorized" errors (HTTP 403)
+ * @class
+ * @extends {CustomError}
+ */
 class UnauthorizedError extends CustomError {
+  /**
+   * Creates an instance of UnauthorizedError
+   * @param message - sent back to client
+   * @param desc - used for logging
+   */
   constructor(message: string, desc: string = "") {
     super(message, 403, desc);
     Object.setPrototypeOf(this, UnauthorizedError.prototype);

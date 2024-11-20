6e957ba512d27719b8916f386ca228a201870636
Milos <m.djuka@hotmail.com>
2024-11-20 20:31:24 +0100

    feat: Add auth middleware for jwt verification

---
+++ src/middlewares/auth.ts
@@ -0,0 +1,72 @@
+import jwt, {
+  JsonWebTokenError,
+  JwtPayload,
+  TokenExpiredError,
+} from "jsonwebtoken";
+import { Request, Response, NextFunction } from "express";
+
+import User from "../entity/User";
+import { UnauthorizedError } from "./CustomError";
+import AppDataSource from "../data-source";
+
+declare module "express" {
+  export interface Request {
+    userId?: number; // Add custom property
+  }
+}
+
+const verifyToken = async (req: Request, res: Response, next: NextFunction) => {
+  const tokenHeader = req.headers["authorization"];
+  const secret = process.env.JWT_SECRET;
+  try {
+    if (!secret) throw new Error("Jwt secret non existent");
+
+    if (!tokenHeader)
+      throw new UnauthorizedError(
+        "Credentials not correct",
+        "No token header in request",
+      );
+
+    const token = tokenHeader.split(" ")[1];
+    if (!token)
+      throw new UnauthorizedError(
+        "Credentials not correct",
+        "No token header in request",
+      );
+
+    let decoded = null;
+    try {
+      decoded = jwt.verify(token, secret) as JwtPayload;
+    } catch (error) {
+      if (error instanceof JsonWebTokenError) {
+        throw new UnauthorizedError(
+          "Credentials not correct",
+          "Invalid token provided",
+        );
+      } else if (error instanceof TokenExpiredError) {
+        throw new UnauthorizedError("Session expired", "Token expired");
+      } else {
+        throw new UnauthorizedError("Credentials not correct");
+      }
+    }
+
+    if (!decoded || !decoded.id || isNaN(parseInt(decoded.id)))
+      throw new UnauthorizedError(
+        "Credentials not correct",
+        "User id not valid",
+      );
+
+    req.userId = decoded.id;
+
+    const userRepository = AppDataSource.getRepository(User);
+    const user = await userRepository.findOneBy({ id: req.userId });
+    if (!user)
+      throw new UnauthorizedError("Credentials not correct", "User not found");
+
+    next();
+  } catch (error) {
+    next(error);
+  }
+};
+
+export { verifyToken };

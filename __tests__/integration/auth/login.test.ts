f15ddbd5a06de712d79bd546cdf2fc28485a30b9
Milos <m.djuka@hotmail.com>
2024-12-03 21:52:08 +0100

    test: Refactor tests to work with AuthService class, and include two fa auth

--- __tests__/integration/auth/login.test.ts
+++ __tests__/integration/auth/login.test.ts
@@ -1,14 +1,17 @@
 import { Application } from "express";
 import request from "supertest";
-import authService from "../../../src/services/auth";
+import AuthService from "../../../src/services/auth";
 import { UnauthorizedError } from "../../../src/middlewares/CustomError";
 import { createApp } from "../../../src/createApp";
 
-jest.mock("../../../src/services/auth"); // Automatically replaces all exports in `userService` with Jest mock functions
+jest.mock("../../../src/services/auth"); // Automatically replaces AuthService methods with Jest mock functions
 
 describe("User Authentication", () => {
   describe("POST /login", () => {
     let app: Application;
+    const mockedAuthService = AuthService as jest.MockedClass<
+      typeof AuthService
+    >;
 
     beforeAll(() => {
       app = createApp();
@@ -19,24 +22,43 @@ describe("User Authentication", () => {
     });
 
     it("should return 200 and a token for valid credentials", async () => {
-      jest.spyOn(authService, "login").mockResolvedValue("fake-token");
+      mockedAuthService.prototype.login.mockResolvedValue({
+        id: 1,
+        email: "test@example.com",
+        firstName: "testName",
+        lastName: "testLastName",
+        profileImage: "testImagePath",
+        jwtToken: "fakeToken",
+      });
 
-      const response = await request(app)
-        .post("/api/v1/auth/login")
-        .send({ email: "test@example.com", password: "password123" });
+      const response = await request(app).post("/api/v1/auth/login").send({
+        email: "test@example.com",
+        password: "password123",
+        twoFaToken: "testTwoFaToken",
+      });
 
       expect(response.status).toBe(200);
-      expect(response.body).toHaveProperty("data", "fake-token");
-      expect(authService.login).toHaveBeenCalledWith(
+      expect(response.body).toHaveProperty("data", {
+        id: 1,
+        email: "test@example.com",
+        firstName: "testName",
+        lastName: "testLastName",
+        profileImage: "testImagePath",
+        jwtToken: "fakeToken",
+      });
+      expect(mockedAuthService.prototype.login).toHaveBeenCalledWith(
         "test@example.com",
         "password123",
+        "testTwoFaToken",
       );
     });
 
     it("should return 400 when email is missing", async () => {
-      const response = await request(app)
-        .post("/api/v1/auth/login")
-        .send({ email: "", password: "password123" });
+      const response = await request(app).post("/api/v1/auth/login").send({
+        email: "",
+        password: "password123",
+        twoFaToken: "testTwoFaToken",
+      });
 
       expect(response.status).toBe(400);
       expect(response.body).toHaveProperty(
@@ -46,9 +68,25 @@ describe("User Authentication", () => {
     });
 
     it("should return 400 when password is missing", async () => {
-      const response = await request(app)
-        .post("/api/v1/auth/login")
-        .send({ email: "test@example.com", password: "" });
+      const response = await request(app).post("/api/v1/auth/login").send({
+        email: "test@example.com",
+        password: "",
+        twoFaToken: "testTwoFaToken",
+      });
+
+      expect(response.status).toBe(400);
+      expect(response.body).toHaveProperty(
+        "error",
+        "All fields must be filled",
+      );
+    });
+
+    it("should return 400 when twoFaToken is missing", async () => {
+      const response = await request(app).post("/api/v1/auth/login").send({
+        email: "test@example.com",
+        password: "password123",
+        twoFaToken: "",
+      });
 
       expect(response.status).toBe(400);
       expect(response.body).toHaveProperty(
@@ -58,19 +96,22 @@ describe("User Authentication", () => {
     });
 
     it("should return 401 for invalid credentials", async () => {
-      jest
-        .spyOn(authService, "login")
-        .mockRejectedValue(new UnauthorizedError("Credentials not correct"));
+      mockedAuthService.prototype.login.mockRejectedValue(
+        new UnauthorizedError("Credentials not correct"),
+      );
 
-      const response = await request(app)
-        .post("/api/v1/auth/login")
-        .send({ email: "test@example.com", password: "wrongpassword" });
+      const response = await request(app).post("/api/v1/auth/login").send({
+        email: "test@example.com",
+        password: "wrongpassword",
+        twoFaToken: "testTwoFaToken",
+      });
 
       expect(response.status).toBe(401);
       expect(response.body).toHaveProperty("error", "Credentials not correct");
-      expect(authService.login).toHaveBeenCalledWith(
+      expect(mockedAuthService.prototype.login).toHaveBeenCalledWith(
         "test@example.com",
         "wrongpassword",
+        "testTwoFaToken",
       );
     });
   });

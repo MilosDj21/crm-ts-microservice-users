ba035a67563b104b8acd07cd14a167c1d8dfc0cb
Milos <m.djuka@hotmail.com>
2024-12-19 20:20:09 +0100

    feat: Add findOne, find, updateOne, delete services for role

--- src/services/role.ts
+++ src/services/role.ts
@@ -1,4 +1,4 @@
-import { Repository } from "typeorm";
+import { Like, Repository } from "typeorm";
 import Role from "../entity/Role";
 import { BadRequestError } from "../middlewares/CustomError";
 
@@ -9,6 +9,25 @@ class RoleService {
     this.roleRepository = roleRepository;
   }
 
+  findOne = async (id: string) => {
+    const idNum = parseInt(id);
+    if (isNaN(idNum)) throw new BadRequestError("Invalid role id");
+
+    const role = await this.roleRepository.findOneBy({ id: idNum });
+    if (!role) throw new Error("Failed to retrieve role from the db");
+
+    return role;
+  };
+
+  find = async (searchTerm: string = "") => {
+    const roles = await this.roleRepository.findBy({
+      name: Like(`%${searchTerm}%`),
+    });
+    if (!roles) throw Error("Failed to retrieve roles from the db");
+
+    return roles;
+  };
+
   saveOne = async (name: string) => {
     const exist = await this.roleRepository.findOneBy({ name });
     if (exist) throw new BadRequestError("Role already exists");
@@ -17,6 +36,25 @@ class RoleService {
       name,
     });
     if (!role) throw new Error("Failed to save role to the db");
+
+    return role;
+  };
+
+  updateOne = async (id: string, name: string) => {
+    const idNum = parseInt(id);
+    if (isNaN(idNum)) throw new BadRequestError("Invalid role id");
+
+    const role = await this.roleRepository.update(idNum, { name });
+    if (!role) throw new Error("Failed to update role in db");
+
+    return role;
+  };
+
+  deleteOne = async (id: string) => {
+    const idNum = parseInt(id);
+    if (isNaN(idNum)) throw new BadRequestError("Invalid role id");
+
+    const role = await this.roleRepository.delete(idNum);
 
     return role;
   };
